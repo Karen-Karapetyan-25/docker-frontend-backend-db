580e6f8a1f0cc88ada3ee5a63f9db6c159b76d9a
Karen-Karapetyan-25 <23karenkarapetyan223@gmail.com>
2025-06-12 20:10:05 +0400

    update

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -51,9 +51,20 @@ data "aws_ami" "ubuntu_22_04" {
   most_recent = true
   owners      = ["099720109477"]   # Canonical
 
-  filter { name = "name"                values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"] }
-  filter { name = "architecture"        values = ["x86_64"] }
-  filter { name = "virtualization-type" values = ["hvm"] }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
 ########################################################################
