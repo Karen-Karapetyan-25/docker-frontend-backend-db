a804b1b45e3152f248c3716e902e0f153e672a3f
Karen-Karapetyan-25 <23karenkarapetyan223@gmail.com>
2025-06-14 14:20:28 +0400

    Change

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -50,12 +50,17 @@ data "aws_ami" "ubuntu_22_04" {
 }
 
 
-
-data "aws_vpc" "default" { default = true }
+data "aws_vpc" "default" { 
+  default = true 
+}
 
 data "aws_subnets" "default" {
-  filter { name = "vpc-id" values = [data.aws_vpc.default.id] }
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
+
 
 
 data "http" "github_meta" {
