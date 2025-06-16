f8ef154f13485b153f55f9d8bf1093789db85391
Karen-Karapetyan-25 <23karenkarapetyan223@gmail.com>
2025-06-16 22:22:48 +0400

    Change

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -184,14 +184,13 @@ resource "aws_instance" "app_host" {
 
 # ─── Outputs ────────────────────────────────────────────────────
 output "ec2_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.app_host.public_ip
+  value = aws_instance.your_instance.public_ip
 }
 
 output "ec2_instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.app_host.id
+  value = aws_instance.your_instance.id
 }
+
 
 output "ec2_public_dns" {
   description = "Public DNS name of the EC2 instance"
