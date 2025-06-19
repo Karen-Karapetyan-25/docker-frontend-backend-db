7d12b44e41d449928b5aa808d61d509754754214
Karen-Karapetyan-25 <23karenkarapetyan223@gmail.com>
2025-06-19 20:19:31 +0400

    Change

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -177,7 +177,6 @@ resource "aws_instance" "app_host" {
   tags = { Name = "app-host" }
 }
 
-# ─── Outputs ────────────────────────────────────────────────────
 output "ec2_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.app_host.public_ip
